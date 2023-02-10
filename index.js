667dc5d7737c9204ce38199f412d814a42dae47e
Karan Thakur <123806385+karanthakur4999@users.noreply.github.com>
2023-02-10 11:47:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
- this is index.js page
+////Todo list App project in Reactjs--
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import "@fortawesome/free-regular-svg-icons";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    <App></App>
+  </>
+);
